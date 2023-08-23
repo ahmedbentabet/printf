437068e959cbd78258e49a6bacd12a0532e0ede5
OthmaneALAOUI <othmane.alaoui.ismaili@gmail.com>
2023-08-22 17:11:00 -0700

    Update main.h

--- main.h
+++ main.h
@@ -7,7 +7,7 @@
 #include <stdarg.h>
 
 int _putchar(char c);
-void print_number(int n);
+void print_number(int n, int *count);
 void print_string(const char *str, int *count);
 int _printf(const char *format, ...);
 
