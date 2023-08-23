0a5ca2579b57f0c7ed292abb1fec63851d36af5c
OthmaneALAOUI <othmane.alaoui.ismaili@gmail.com>
2023-08-22 17:59:40 -0700

    Update task 0

--- _printf.c
+++ _printf.c
@@ -20,11 +20,13 @@ int _printf(const char *format, ...)
 			switch (*format)
 			{
 				case 'c':
-					char ch = (char)va_arg(args, int);
+					{
+						char c = (char)va_arg(args, int);
 
-					_putchar(ch);
-					count++;
-					break;
+						_putchar(c);
+						count++;
+						break;
+					}
 				case 's':
 					print_string(va_arg(args, char *), &count);
 					break;
@@ -54,4 +56,3 @@ int _printf(const char *format, ...)
 	return (count);
 
 }
-
